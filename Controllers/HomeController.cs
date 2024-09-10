5ea3cd466c8898643e00b37de7d28824e909a264
Zinedean Meaker Saaiman <ST10348753@vcconnect.edu.za>
2024-09-10 20:55:22 +0200

    Added Submit Claim View and Controller methods for lecturers to submit their claims.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,21 @@ namespace ST10348753_PROG6212POE.Controllers
             return View();
         }
 
+        public IActionResult SubmitClaim()
+        {
+            // This displays the form where lecturers submit their claims
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmitClaim(string hoursWorked)
+        {
+            // For now, we're just simulating a submission and showing a message
+            ViewBag.Message = $"Claim for {hoursWorked} hours submitted.";
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
