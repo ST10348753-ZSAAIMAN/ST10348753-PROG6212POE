40bbaae240f1e7ce6667f4c3e25193c44a08c087
Zinedean Meaker Saaiman <ST10348753@vcconnect.edu.za>
2024-09-10 18:00:29 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10348753_PROG6212POE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
